309c5334a3b1f8a9b823f95b13fcc469e5400d73
cristian-albu <albucristian95p@gmail.com>
2024-05-30 23:28:10 +0300

    search functionality and data from server refactor

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const POKEMON_LIST_OFFSET = 20;
+export const POKEMON_MAX_COUNT = 20;
 
 export const SPRITES_HOST =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
